a69253a79d1c5d7fb71af40e29a5aa8bf7efb3c2
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-13 18:25:18 -0300

    feat:login google and facebook (configure the build afterwards)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
-import NextAuth from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
@@ -9,10 +10,12 @@ export default NextAuth({
       clientSecret: process.env.FACEBOOK_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-      asdasdasd
-
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-  ]
-})
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,58 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
+  // console.log(session);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className={`${inter.className} text-4xl font-semibold`}>
-        HELLO NEXT {"<3"}
+        Hello {session?.user?.name}
       </h1>
+
+      {!!session ? (
+        <>
+          <link rel="preload" as="image" href={session?.user?.image!}></link>
+          <Image
+            loader={() => session?.user?.image!}
+            src={session?.user?.image!}
+            alt={session?.user?.name!}
+            width="200"
+            height="200"
+            unoptimized
+          />
+          <button
+            className="bg-red-500 p-3 text-white"
+            onClick={() => signOut()}
+          >
+            Sair
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>Registre-se ou faça o login</h2>
+
+          <button
+            className="bg-blue-500 p-3 text-white"
+            onClick={() => signIn()}
+          >
+            Entrar
+          </button>
+        </>
+      )}
     </main>
   );
+}
+
+//remove undefined error on console react devtools backend
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props: { session },
+  };
 }
