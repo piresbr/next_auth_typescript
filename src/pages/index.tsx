4628789e2861d1896e3140272b9a2bc62f813988
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-13 15:18:25 -0300

    fix:format documents with eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className={`${inter.className} text-4xl font-semibold`}>
-        HELLO NEXT {'<3'}
+        HELLO NEXT {"<3"}
       </h1>
     </main>
-  )
+  );
 }
--- src/utils/connectDb.ts
+++ src/utils/connectDb.ts
@@ -3,37 +3,46 @@ import mongoose from "mongoose";
 // https://stackoverflow.com/questions/75206870/nextjs-mongoose-mongo-atlas-multiple-connections-even-with-caching
 
 if (!process.env.DATABASE_URL) {
-    throw new Error("A conexão com seu banco de dados não foi encontrada. Cheque suas configurações e tente novamente. ")
+  throw new Error(
+    "A conexão com seu banco de dados não foi encontrada. Cheque suas configurações e tente novamente. "
+  );
 }
 
 const DATABASE_URL: string = process.env.DATABASE_URL;
 
 let globalWithMongoose = global as typeof globalThis & {
-    mongoose: any;
-}
+  mongoose: any;
+};
 
 let cached = globalWithMongoose.mongoose;
 
 if (!cached) {
-    cached = globalWithMongoose.mongoose = { conn: null, promise: null };
+  cached = globalWithMongoose.mongoose = { conn: null, promise: null };
 }
 
 async function connectDb() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise) {
-        const options = { bufferCommands: false, useNewUrlParser: true, useUnifiedTopology: true };
-
-        cached.promise = mongoose.connect(DATABASE_URL, options).then((mongoose) => {
-            console.log('Conexão feita com sucesso ')
-            return mongoose;
-        }).catch((error) => {
-            console.log(error as Error)
-            return
-        })
-    }
-    cached.conn = await cached.promise;
+  if (cached.conn) {
     return cached.conn;
+  }
+  if (!cached.promise) {
+    const options = {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose
+      .connect(DATABASE_URL, options)
+      .then((mongoose) => {
+        console.log("Conexão feita com sucesso ");
+        return mongoose;
+      })
+      .catch((error) => {
+        console.log(error as Error);
+        return;
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 export default connectDb;
