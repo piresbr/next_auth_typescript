99baeb6fb4f5e1f9e4f3c2cf038a250bca0b52f2
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-14 19:09:45 -0300

    feat: page auth structure

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
 export default function auth() {
-  return <div>Auth</div>;
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div className="w-full h-100 flex items-center justify-center">
+        {/*----Form----*/}
+        <div className="w-full sm:w5/6 md:w-2/3 lg:w1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex flex-col items-center justify-center">
+          <div className="w-full flex items-center justify-between px-12">
+            <div className="w-full px-12 py-4">
+              <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">
+                Registre-se
+              </h2>
+              <p className="text-center text-sm text-gray-600 mt-2">
+                Você tem uma conta?
+                <Link className="text-bold text-blue-500 ml-1" href="/register">
+                  Clique aqui para entrar
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
