8c12af7e6a5da272926de0a332ba6eef3e0dcada
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-15 20:41:39 -0300

    style: input form style component

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -5,11 +5,23 @@ interface IInputProps {
   label: string;
   type: string;
   icon: JSX.Element;
+  placeholder: string;
 }
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
-  const { name, label, type, icon } = props;
-  return <></>;
+  const { name, label, type, icon, placeholder } = props;
+  return (
+    <div className="relative mt-1 rounded-md shadow-sm">
+      <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
+        <span className="text-gray-500 text-sm">{icon}</span>
+      </div>
+      <input
+        type={type}
+        className="w-full ру-2 pl-8 pr-7 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm"
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 
 export default Input;
