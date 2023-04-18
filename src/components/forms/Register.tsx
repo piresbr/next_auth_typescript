57f7fe6b760a705a0a69bd282e26d3a28a6f0607
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-18 11:26:25 -0300

    styles: grid register form

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -1,19 +1,31 @@
 import * as React from "react";
 import Input from "../inputs/Input";
 import { CiUser } from "react-icons/ci";
+import { FiMail } from "react-icons/fi";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface IRegisterProps {}
 
 const FormSchema = z.object({
   first_name: z
     .string()
-    .min(2, "Primeiro nome deve ser maior que dois caracteres")
+    .min(2, "Primeiro nome deve ser maior que 2 caracteres")
     .max(50, "O primeiro nome deve ter no máximo 50 caracteres")
     .regex(
-      new RegExp("^[a-zA-Z]+$", "O nome não pode ter caracteres especiais")
+      new RegExp("^[a-zA-Z]+$"),
+      "O nome não pode ter caracteres especiais"
     ),
+  last_name: z
+    .string()
+    .min(2, "Sobrenome deve ser maior que 2 caracteres")
+    .max(70, "O sobrenome deve ter no máximo 70 caracteres")
+    .regex(
+      new RegExp("^[a-zA-Z]+$"),
+      "O nome não pode ter caracteres especiais"
+    ),
+  email: z.string().email("Email inválido"),
 });
 
 type FormSchemaProps = z.infer<typeof FormSchema>;
@@ -31,17 +43,46 @@ const Register: React.FunctionComponent<IRegisterProps> = (props) => {
 
   return (
     <form className="my-8 text-sm" onSubmit={handleSubmit(onSubmit)}>
-      <div className="gap-2 md:flex">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-3">
         <Input
           name="first_name"
           label="First name"
           type="text"
+          className="col-span-1 sm:col-span-1 relative"
           placeholder="Primeiro nome"
           icon={<CiUser />}
           register={register}
           error={errors?.first_name?.message}
           disabled={isSubmitting}
         />
+        <Input
+          name="last_name"
+          label="Last name"
+          type="text"
+          className="col-span-1 sm:col-span-1 relative"
+          placeholder="Sobrenome"
+          icon={<CiUser />}
+          register={register}
+          error={errors?.last_name?.message}
+          disabled={isSubmitting}
+        />
+        <Input
+          name="email"
+          label="Email"
+          type="text"
+          className="col-span-1 sm:col-span-2 relative"
+          placeholder="joao@exemplo.com"
+          icon={<FiMail />}
+          register={register}
+          error={errors?.email?.message}
+          disabled={isSubmitting}
+        />
+        <button
+          type="submit"
+          className="col-span-1 sm:col-span-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-700 hover:transition-all hover:duration-200"
+        >
+          Cadastrar
+        </button>
       </div>
     </form>
   );
--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -6,6 +6,7 @@ interface IInputProps {
   name: string;
   label: string;
   type: string;
+  className?: string;
   icon: JSX.Element;
   placeholder: string;
   register: any;
@@ -14,37 +15,42 @@ interface IInputProps {
 }
 
 const Input: React.FunctionComponent<IInputProps> = (props) => {
-  const { name, label, type, icon, placeholder, register, error, disabled } =
-    props;
+  const {
+    name,
+    label,
+    type,
+    className,
+    icon,
+    placeholder,
+    register,
+    error,
+    disabled,
+  } = props;
   return (
-    <div className="mt-3 w-full">
+    <div className={`${className}`}>
       <label htmlFor={name} className="text-gray-500">
         {label}
       </label>
       <div className="relative mt-1 rounded-md shadow-sm">
         <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
-          <span
-            className={`text-gray-500 text-sm ${
-              error ? "translate-y-[-10px]" : ""
-            }`}
-          >
-            {icon}
-          </span>
+          <span className="text-gray-500 text-sm">{icon}</span>
         </div>
         <input
           type={type}
-          className="w-full ру-2 pl-8 pr-7 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm"
+          className={`w-full mb-1 py-2 pl-8 pr-2 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm`}
           placeholder={placeholder}
           {...register(name)}
-          style={{ borderColor: `${error ? "#ED4337" : ""}` }}
+          style={{ border: `${error ? "1px solid #ED4337" : ""}` }}
         />
-        {error && (
-          <div className="fill-red-500 absolute right-2 text-xl">
-            <IoAlertCircle fill="#ED4337" />
-          </div>
-        )}
-        {error && <p className="text-[#ED4337]">{error}</p>}
       </div>
+      {error && (
+        <div className="fill-red-500 absolute right-1 text-xl">
+          <IoAlertCircle fill="#ED4337" />
+        </div>
+      )}
+      {error && (
+        <p className="text-[#ED4337] max-w-[calc(100%_-_28px)]">{error}</p>
+      )}
     </div>
   );
 };
