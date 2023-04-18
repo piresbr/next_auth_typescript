e79149aa39ce8d11f36a87ad81408de59472f9af
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-18 09:57:55 -0300

    styles: finish input style

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -17,21 +17,34 @@ const Input: React.FunctionComponent<IInputProps> = (props) => {
   const { name, label, type, icon, placeholder, register, error, disabled } =
     props;
   return (
-    <div className="relative mt-1 rounded-md shadow-sm">
-      <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
-        <span className="text-gray-500 text-sm">{icon}</span>
-      </div>
-      <input
-        type={type}
-        className="w-full ру-2 pl-8 pr-7 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm"
-        placeholder={placeholder}
-        {...register(name)}
-      />
-      {error && (
-        <div className="fill-red-500 absolute right-2 text-xl">
-          <IoAlertCircle fill="#ED4337" />
+    <div className="mt-3 w-full">
+      <label htmlFor={name} className="text-gray-500">
+        {label}
+      </label>
+      <div className="relative mt-1 rounded-md shadow-sm">
+        <div className="pointer-event-none absolute left-0 inset-y-0 flex items-center pl-3">
+          <span
+            className={`text-gray-500 text-sm ${
+              error ? "translate-y-[-10px]" : ""
+            }`}
+          >
+            {icon}
+          </span>
         </div>
-      )}
+        <input
+          type={type}
+          className="w-full ру-2 pl-8 pr-7 block rounded-md border border-gray-300 outline-offset-2 outline-transparent focus:border-blue-500 focus:ring-indigo-500 focus:ring-2 text-sm"
+          placeholder={placeholder}
+          {...register(name)}
+          style={{ borderColor: `${error ? "#ED4337" : ""}` }}
+        />
+        {error && (
+          <div className="fill-red-500 absolute right-2 text-xl">
+            <IoAlertCircle fill="#ED4337" />
+          </div>
+        )}
+        {error && <p className="text-[#ED4337]">{error}</p>}
+      </div>
     </div>
   );
 };
