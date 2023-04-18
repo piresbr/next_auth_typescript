a830257ba1855081e1daa14b92d797e2ff240114
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-18 10:07:42 -0300

    feat: zod validation form

--- src/components/forms/Register.tsx
+++ src/components/forms/Register.tsx
@@ -2,8 +2,21 @@ import * as React from "react";
 import Input from "../inputs/Input";
 import { CiUser } from "react-icons/ci";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface IRegisterProps {}
+
+const FormSchema = z.object({
+  first_name: z
+    .string()
+    .min(2, "Primeiro nome deve ser maior que dois caracteres")
+    .max(50, "O primeiro nome deve ter no máximo 50 caracteres")
+    .regex(
+      new RegExp("^[a-zA-Z]+$", "O nome não pode ter caracteres especiais")
+    ),
+});
+
+type FormSchemaProps = z.infer<typeof FormSchema>;
 
 const Register: React.FunctionComponent<IRegisterProps> = (props) => {
   const {
@@ -11,7 +24,9 @@ const Register: React.FunctionComponent<IRegisterProps> = (props) => {
     handleSubmit,
     watch,
     formState: { errors, isSubmitting },
-  } = useForm();
+  } = useForm<FormSchemaProps>({
+    resolver: zodResolver(FormSchema),
+  });
   const onSubmit = (data: any) => console.log(data);
 
   return (
