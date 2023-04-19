5e44f86934f43e48d86c825c9600b3c1d75dfa52
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-19 19:31:41 -0300

    feat: install package axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function App({
   Component,
@@ -13,6 +14,18 @@ export default function App({
         <Head>
           <title>{session?.user?.name!}</title>
         </Head>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </>
