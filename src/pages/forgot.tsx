9920a897acd231320b79be46628f2a1ba93d1ec1
Bruno Pires <bruno.pires.1096@gmail.com>
2023-04-22 03:17:32 -0300

    refactor: function forgot

--- src/pages/forgot.tsx
+++ src/pages/forgot.tsx
@@ -1,24 +1,12 @@
 import ForgotForm from "@/components/forms/Forgot";
-import { NextPageContext } from "next";
-import { getCsrfToken, getProviders } from "next-auth/react";
 
-export default function auth({
-  tab,
-  callbackUrl,
-  csrfToken,
-  providers,
-}: {
-  tab: string;
-  callbackUrl: string;
-  csrfToken: string;
-  providers: any;
-}) {
+export default function forgot() {
   return (
     <div className="w-full flex items-center justify-center">
       <div className="w-full min-h-screen flex items-center justify-center bg-slate-100">
         {/*----Form----*/}
         <div className="w-5/6 md:w-2/3 lg:w1/2 xl:w-2/4 2xl:w-1/3 h-full bg-white flex flex-col items-center justify-center rounded-md py-6">
-          <div className="w-full flex items-center justify-between px-12">
+          <div className="w-full flex items-center justify-between px-4 lg:px-6">
             <div className="w-full p-6">
               <ForgotForm />
             </div>
@@ -27,23 +15,4 @@ export default function auth({
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps(ctx: NextPageContext) {
-  const { req, query } = ctx;
-  const tab = query.tab ? query.tab : "signin";
-  const callbackUrl = query.callbackUrl
-    ? query.callbackUrl
-    : process.env.NEXTAUTH_URL;
-
-  const csrfToken = await getCsrfToken(ctx);
-  const providers = await getProviders();
-  return {
-    props: {
-      providers: Object.values(providers!),
-      tab: JSON.parse(JSON.stringify(tab)),
-      callbackUrl,
-      csrfToken,
-    },
-  };
 }
